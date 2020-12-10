ca921fc7d3e845e5871225612ee55f5d41fb7c68
kritika200015 <krjjain14@gmail.com>
2020-12-10 19:17:59 +0530

    effiu

--- boyfriend.py
+++ boyfriend.py
@@ -4,12 +4,14 @@ import random
 account_sid = 'ACbf8c97ce07c86f7b8033f6ea64b59d4d' 
 auth_token = '1b1c07cd85f39e4a771a3f7cc89dcf8c' 
 client = Client(account_sid, auth_token) 
-messages = ["I love you","You are really beautiful","One day you will find someone","Try your best babygirl"]
+def trying():
+ messages = ["I love you","You are really beautiful","One day you will find someone","Try your best babygirl"]
+ print(random.choice(messages))
 
 def send_message():
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body= print(random.choice(messages)),
+                              body= trying,
                               to='whatsapp:+919643882723' 
                             ) 
  
