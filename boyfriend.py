f35ad33510aaac344979654950a7878e6852b5fb
kritika200015 <krjjain14@gmail.com>
2020-12-10 19:37:13 +0530

    final

--- boyfriend.py
+++ boyfriend.py
@@ -1,5 +1,5 @@
 from twilio.rest import Client
-import random 
+
  
 account_sid = 'ACbf8c97ce07c86f7b8033f6ea64b59d4d' 
 auth_token = '1b1c07cd85f39e4a771a3f7cc89dcf8c' 
@@ -9,7 +9,7 @@ client = Client(account_sid, auth_token)
 def send_message():
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body=print(random.choice(["test","testing","testimonial"])),
+                              body= 'remember that one day everything will be fine and you will find me in a person till then I love you ;)',
                               to='whatsapp:+919643882723' 
                             ) 
  
